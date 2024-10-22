ffe629a487e145d10b7fc034aed3e0f1edf0a78d
diallo_123 <almahadydiallo@gmail.com>
2024-10-22 16:33:12 +0100

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  mode: "jit",
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: "#050816",
+        secondary: "#aaa6c3",
+        tertiary: "#151030",
+      },
+      boxShadow: {
+        card: "0px 35px 120px -15px #211e35",
+      },
+      screens: {
+        xs: "450px",
+      },
+    },
+  },
+  plugins: [],
+};
